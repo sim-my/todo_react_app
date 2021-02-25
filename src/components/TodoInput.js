853b4c20cd638e601a8eceab11f89aca7c03c3ca
sim-my <simranpanthi101@gmail.com>
2021-02-25 20:02:17 +0545

    Created react skeleton

---
+++ src/components/TodoInput.js
@@ -0,0 +1,10 @@
+const TodoInput = () => {
+  return (
+    <div>
+      <input className="todo-input" type="text" />
+      <button type="button">Add</button>
+    </div>
+  );
+};
+
+export default TodoInput;
